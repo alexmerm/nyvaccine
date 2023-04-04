0d8991d2c74a2476afa0b0aa5ab3edf6e3003e1c
alexmerm <1852285+alexmerm@users.noreply.github.com>
2023-04-04 16:11:31 -0400

    - removed CVS files - implemented loading from location

--- nystatechecker.py
+++ nystatechecker.py
@@ -2,16 +2,16 @@
 import requests
 from pprint import pp as pprint
 from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
 import sys
 import json,yaml
 
 CONFIG_FILE = 'config.yaml'
+LOCATIONS_FILE = 'locations.yaml'
 
 
 ENDPOINT = "https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers"
-# ENDPOINT = "http://api.scraperapi.com?api_key="+ config['scraperapi']['api_key'] + "&url=https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers"
 
-TOCHECK = ["Jones Beach - Field 3", "SUNY Stony Brook University Innovation and Discovery Center", "Aqueduct Racetrack - Racing Hall","Javits Center","Westchester County Center", "Suffolk CCC - Brentwood", "SUNY Old Westbury", "Stony Brook - Southampton"]
 AVAILABLE_CODE = "Y"
 NOT_AVAILABLE_CODE = "N"
 
@@ -45,34 +45,45 @@ def getApptsTest():
     return {'providerList': [{'providerName': 'Javits Center', 'address': 'New York, NY', 'availableAppointments': 'Y'}, {'providerName': 'Jones Beach - Field 3', 'address': 'Wantagh, NY', 'availableAppointments': 'N'}, {'providerName': 'State Fair Expo Center: NYS Fairgrounds', 'address': 'Syracuse, NY', 'availableAppointments': 'N'}, {'providerName': 'SUNY Albany ', 'address': 'Albany, NY', 'availableAppointments': 'N'}, {'providerName': 'Westchester County Center', 'address': 'White Plains, NY', 'availableAppointments': 'N'}, {'providerName': 'SUNY Stony Brook University Innovation and Discovery Center', 'address': 'Stony Brook, NY', 'availableAppointments': 'N'}, {'providerName': 'SUNY Potsdam Field House', 'address': 'Potsdam, NY', 'availableAppointments': 'AA'}, {'providerName': 'Aqueduct Racetrack - Racing Hall', 'address': 'South Ozone Park, NY', 'availableAppointments': 'N'}, {'providerName': 'Plattsburgh International Airport -Connecticut Building', 'address': 'Plattsburgh, NY', 'availableAppointments': 'N'}, {'providerName': 'SUNY Binghamton', 'address': 'Johnson City, NY', 'availableAppointments': 'N'}, {'providerName': 'SUNY Polytechnic Institute - Wildcat Field House', 'address': 'Utica, NY', 'availableAppointments': 'N'}, {'providerName': 'University at Buffalo South Campus - Harriman Hall', 'address': 'Buffalo, NY', 'availableAppointments': 'N'}, {'providerName': 'Rochester Dome Arena', 'address': 'Henrietta, NY', 'availableAppointments': 'N'}], 'lastUpdated': '2/11/2021, 8:01:37 PM'}
 
 
-#Takes dict of availablity (appointments) from endpoint and returns arr of places with available appointment that have been preselected to look at 
-def availableLocations(appointments):
+#Takes dict of loctions to check and dict of availablity (appointments) from endpoint and returns arr of places with available appointment that have been preselected to look at 
+def availableLocations(locations_to_check, appointments):
     availableLocs = []
     for loc in appointments['providerList']:
-        if loc['providerName'] in TOCHECK:
-            pprint(loc)
+        if loc['providerName'] in locations_to_check and locations_to_check[loc['providerName']]:
             if loc['availableAppointments'] == AVAILABLE_CODE:
                 availableLocs.append(loc['providerName'])
     return availableLocs
 
+#Takes list of locations where vaccine are available
+#Sends pushover messages and Twilio texts with information
 def notify(locs):
     message = 'Vaccine Appts Available at:\n'
     for name in locs:
         message = message + name + '\n'
-    print(message)
+    if(DEBUG):
+        print(message)
     pushover(message)
     sendTexts(message)
 
+#Sends texts to numbers in config['twilio']['numbers']
 def sendTexts(message):
     client = Client(config['twilio']['acct_sid'],config['twilio']['auth_token'])
     for num in config['twilio']['numbers']:
-        msg = client.messages.create(
-            to=num,
-            from_=config['twilio']['from_number'],
-            body=message
-        )
-        print(msg)
+        try:
+            msg = client.messages.create(
+                to=num,
+                from_=config['twilio']['from_number'],
+                body=message
+            )
+            if(DEBUG):
+                print(msg.status)
+        except TwilioRestException as err:
+            print("Twilio Error, Run with \"debug:true\" for more details ")
+            if(DEBUG):
+                print(err)
 
+
+#Sends pushover notifciations
 def pushover(message):
     #Pushover Request
     r = requests.post("https://api.pushover.net/1/messages.json", data = {
@@ -83,7 +94,13 @@ def pushover(message):
         "message": message,
         "priority" : 1
     })
-    print(r.text)
+    if r.status_code == 200:
+        if DEBUG:
+            print("Pushover Successful")
+    else:
+        print("Pushover Error, Run with \"debug:true\" for more details")
+        if DEBUG:
+            print(r.json())
 
 
 
@@ -92,15 +109,20 @@ def pushover(message):
 
 
 #Loads Config file from CONFIG_FILE
-def loadConfig():
-    with open(CONFIG_FILE,'r') as f:
+def loadConfig(path):
+    with open(path,'r') as f:
+        return yaml.safe_load(f)
+
+#Loads dict of locations to check
+def loadLocations(path):
+    with open(path, 'r') as f:
         return yaml.safe_load(f)
 
 
 def main():
     #Load Config
     global config
-    config = loadConfig()
+    config = loadConfig(CONFIG_FILE)
 
     global DEBUG
     if('debug' in config):
@@ -112,14 +134,19 @@ def main():
         print("DEBUG MODE")
         pprint(config)
 
+    #load locations to check
+    to_check = loadLocations(LOCATIONS_FILE)
+
     #Pull Appts
     appts = getApptsTest()
 
 
     #Determine which Places have available locations
-    availableLocs = availableLocations(appts)
-    print(availableLocs)
+    availableLocs = availableLocations(to_check, appts)
+    print("AVAILABLE LOCATIONS : " + str(availableLocs))
+    #If Locations are available notify
     if(len(availableLocs) > 0):
         notify(availableLocs)
 
-main()
+if __name__ == "__main__":
+    main()
